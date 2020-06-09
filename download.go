3047a737667b7503b552bbd7634c732d0c8828ae
winebarrel <sugawara@winebarrel.jp>
2020-06-09 19:31:39 +0900

    Add retry

--- download.go
+++ download.go
@@ -1,8 +1,11 @@
 package rds_download_log_file
 
 import (
+	"fmt"
 	"io"
+	"strings"
 
+	"github.com/cenkalti/backoff"
 	"github.com/crowdmob/goamz/aws"
 	"github.com/crowdmob/goamz/rds"
 )
@@ -11,27 +14,42 @@ func Donwload(region, dbInstanceIdentifier, logFileName string, writer io.Writer
 	auth, err := aws.EnvAuth()
 
 	if err != nil {
-		return err
+		return fmt.Errorf("AWS env auth error: %w", err)
 	}
 
 	svc, err := rds.New(auth, aws.GetRegion(region))
 
 	if err != nil {
-		return err
+		return fmt.Errorf("RDS client create error: %w", err)
 	}
 
-	reader, err := svc.DownloadCompleteDBLogFile(dbInstanceIdentifier, logFileName)
+	ticker := backoff.NewTicker(backoff.NewExponentialBackOff())
 
-	if err != nil {
-		return err
+	for _ = range ticker.C {
+		var reader io.ReadCloser
+		reader, err = svc.DownloadCompleteDBLogFile(dbInstanceIdentifier, logFileName)
+
+		if err != nil {
+			if strings.Contains(err.Error(), "Body: Rate exceeded") {
+				continue
+			}
+
+			return fmt.Errorf("log download error: %w", err)
+		}
+
+		defer reader.Close()
+		_, err = io.Copy(writer, reader)
+
+		if err != nil {
+			continue
+		}
+
+		ticker.Stop()
+		break
 	}
 
-	defer reader.Close()
-
-	_, err = io.Copy(writer, reader)
-
 	if err != nil {
-		return err
+		return fmt.Errorf("buffer copy error: %w", err)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module rds_download_log_file
 go 1.14
 
 require (
+	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/crowdmob/goamz v0.0.0-20150128194925-3a06871fe9fc
 	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
 )
